4d057d0f888b155887da11347890026eff81f1e4
Dushyanth Jyothi <Dushyanth.Jyothi@ebi.ac.uk>
2017-02-12 18:24:02 +0000

    ab

---
+++ ab/src/bb.py
@@ -0,0 +1,14 @@
+'''
+Created on 12 Feb 2017
+
+@author: djyothi
+'''
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+        y = 5
+        return y +a
+
+print add(1,2)
+print addFixedValue(1)
